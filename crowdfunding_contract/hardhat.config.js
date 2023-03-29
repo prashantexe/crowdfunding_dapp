4b9bcdaba62a632e695a4f80ec4dd72a881da0d9
Prashant <prashantd049@gmail.com>
2023-03-29 17:19:44 +0530

    Deployed the contract

--- crowdfunding_contract/hardhat.config.js
+++ crowdfunding_contract/hardhat.config.js
@@ -6,8 +6,8 @@ module.exports = {
     networks:{
       hardhat: {},
       goreli:{
-        url: "https://rpc.ankr.com/eth_goerli"
-
+        url: "https://rpc.ankr.com/eth_goerli",
+        accounts: [`0x${process.env.PRIVATE_KEY}`]
       }
     },
     settings: {
